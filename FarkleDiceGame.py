ca206b329f65246a14b1f0f9277876f4c155ea55
Ashley Johnson <wt6268bu@go.minneapolis.edu>
2019-01-23 12:19:59 -0600

    Initial commit

---
+++ FarkleDiceGame.py
@@ -0,0 +1,135 @@
+# Author: Ashley Johnson
+# Program: Farkle Dice Game
+
+# This is the main controller. Automated functions of rolls and scoring are in the Turn module.
+
+import Turn
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Player:
+    name: str
+    score: int
+    turn_score: int
+    turn: bool
+    file: str
+
+    print('''
+                                              Let's Play Farkle! 
+                                            First to 10,000 WINS!
+        
+                                *************** GAME RULES *******************
+        
+        Each Roll must score. Once a score of 500 is reached, a player can choose to stop rolling and keep the points, 
+        or risk it all for more points. If a player does not score any points on a roll it is a bust; all points 
+        lost and becomes the computer's turn
+    
+        Points are scored by rolling:
+            1s               100
+            5s                50
+        3 of a kind      # of die x 100
+        4 of a kind      3 of a kind x2
+        5 of a kind      4 of a kind x2
+        6 of a kind      5 of a kind x2
+         
+        Get all six 1s on the first roll for an INSTANT WIN!''')
+
+
+    # assign players
+
+player_name = input("What is your name?: ")
+human = Player(player_name, 0, 0, True, 'player.txt')
+computer = Player("Computer", 0, 0, False, 'computer.txt')
+win = False
+
+# While no one has 10,000 points, play the game.
+while win == False:
+
+    # initialize all 6 dice for first roll.
+    comp_rolling = []
+    human_rolling = []
+
+    while human.turn:
+        rolled_dice = Turn.roll(human_rolling)
+        rolled_score, non_scoring = Turn.score(rolled_dice)
+
+        print(human.name + "! You rolled: ")
+        print(rolled_dice)
+
+        if rolled_score == 0:
+            print("\nBUST! Your turn is over")
+            human.turn_score = 0
+            computer.turn = True
+            human.turn = False
+            break
+
+        print("\nCurrent Rolled Score: " + str(rolled_score))
+
+        if rolled_score >= 500:
+            keep = input("Do you want to keep this score, and end your turn?(y/n): ")
+            keep.lower()
+            if keep == "y":
+                Turn.save(human.file, (rolled_score + human.turn_score))
+                human.score = Turn.total(human.file)
+                print("\n Your current score is: " + str(human.score))
+                human.turn_score = 0
+                computer.turn = True
+                human.turn = False
+                break
+
+        str_dice = input("List the dice you'd like to keep, separated by comma: ")
+        temp_dice = str_dice.split(',')
+        saved_dice = [int(num) for num in temp_dice]
+        human.turn_score += Turn.score(saved_dice)
+
+        human_rolling = saved_dice
+
+    while computer.turn:
+        rolled_dice = Turn.roll(comp_rolling)
+        rolled_score, non_scoring = Turn.score(rolled_dice)
+
+        print(computer.name + " rolled: ")
+        print(rolled_dice)
+
+        if rolled_score == 0:
+            print("\nBUST! Your turn is over")
+            computer.turn_score = 0
+            computer.turn = False
+            human.turn = True
+            break
+
+        print("\nCurrent Rolled Score: " + str(rolled_score))
+
+        if rolled_score >= 500:
+            temp_score = rolled_score + computer.turn_score + computer.score
+
+            if temp_score > human.score:
+                Turn.save(computer.file, (rolled_score + computer.turn_score))
+                computer.score = Turn.total(computer.file)
+                print("\n Computer current score is: " + str(computer.score))
+                computer.turn_score = 0
+                computer.turn = False
+                human.turn = True
+                break
+
+        for n in range(len(non_scoring)):
+            for i in range(len(rolled_dice)):
+                if non_scoring[n].index == rolled_dice[i].index:
+                    rolled_dice.remove(rolled_dice[i])
+
+        computer.turn_score += Turn.score(rolled_dice)
+
+        comp_rolling = rolled_dice
+
+    print("\n Current Scores: Player: " + str(human.score) + " Computer: " + str(computer.score))
+
+    if human.score >= 10000:
+        print("You won! Congratulations!")
+        win = True
+        Turn.endgame()
+    if computer.score >= 10000:
+        print("You lost. Better luck next time!")
+        win = True
+        Turn.endgame()
---
+++ Turn.py
@@ -0,0 +1,120 @@
+# Ashley Johnson
+# A separate module for the functions of a single turn of a player
+
+import random
+from dataclasses import dataclass
+
+
+@dataclass
+class Die:
+    index: int
+    value: int
+
+# define a simple function to roll and return one dice.
+
+
+def roll_die():
+    one_die = random.randint(1, 6)
+    return one_die
+
+
+def score(current_roll):
+    sets_score = {1: 1000, 2: 200, 3: 300, 4: 400, 5: 500, 6: 600}
+
+    multiplier = {4: 2, 5: 3, 6: 4}
+
+    die_score = {1: 100, 2: 0, 3: 0, 4: 0, 5: 50, 6: 0}
+
+    dice_score = 0
+
+    dice_roll = current_roll
+    match = []
+    dice_left = []
+    no_score = []
+
+    auto_win = [1, 1, 1, 1, 1, 1]
+    straight = [1, 2, 3, 4, 5, 6]
+
+
+    # if there are 6 dice, check if it is an automatic win, if so return 100000 to signal auto win
+    if dice_roll == auto_win:
+        return 100000
+    # if not ten thousand, then check for a straight.
+    if dice_roll == straight:
+        return 1500
+
+    # check for 3 or more of a kind using a nested loop. The outer loop checks each die value against the inner loop of
+    # dice rolls. If the roll matches the die number,
+
+    for a in dice_roll:
+        match = [b for b in dice_roll if b.value == a.value]
+
+
+    # based on quantity of matches
+    match_qty = len(match)
+    match_val = match[0]
+    print(match_val)
+
+    if match_qty > 3:
+        dup_score = sets_score.get(match_val)
+        dup_multi = multiplier.get(match_qty)
+        dice_score += dup_score*dup_multi
+        dice_left = [c for c in dice_roll if c.value != match_val]
+
+    for e in dice_left:
+        single_val = die_score.get(e.value)
+        if single_val > 0:
+            dice_score += single_val
+        else:
+            no_score += [e.value]
+
+    return dice_score, no_score
+
+
+def roll(num_of_dice):
+
+    # determine number of dice to be rolled based on how many dice were saved
+    to_roll = (6 - (len(num_of_dice)))
+
+    # declare a blank list to assign the new dice rolls to, if it is their first roll assign blank to current roll.
+    another_roll = []
+
+    # loop through each die roll
+    for f in range(to_roll):
+        another_roll += [roll_die()]
+
+    another_roll.sort()
+    g = 0
+    d_roll = []
+
+    for j in another_roll:
+        new_die = Die(j)
+        d_roll += [new_die]
+        g += 1
+
+    return d_roll, another_roll
+
+
+def save(filename, addScore):
+    save_file = open(filename, 'a')
+    save_file.write(str(addScore) + '\n')
+    save_file.close()
+
+
+def total(filename):
+    open_file = open(filename, 'r')
+    game_total = 0
+    score_list = open_file.readlines()
+    num_scores = len(score_list)
+    open_file.close()
+
+    for k in range(num_scores):
+        cur_score = int(score_list[k])
+        game_total += cur_score
+
+    return game_total
+
+
+def endgame():
+    open('player.txt', 'w').close()
+    open('computer.txt', 'w').close()
